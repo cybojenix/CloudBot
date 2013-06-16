60d4742e7fff15a7097489a2f07b61f15223afa8
cybojenix <anthonydking@gmail.com>
2013-06-16 17:13:39 +0100

    let's bump up the security around the new method

--- plugins/8ball.py
+++ plugins/8ball.py
@@ -13,8 +13,9 @@ with open("plugins/data/8ball_responses.txt") as f:
         f.readlines()if not line.startswith("//")]
 
 
+
 @hook.command('8ball')
-def eightball(inp, conn=None, chan=None, nick=''):
+def eightball(inp, conn=None, chan=None, nick='', mask='', bot=None, notice=None):
     "8ball [channel] <question> -- The all knowing magic eight ball, " \
     "in electronic form. Ask and it shall be answered!" \
     "If [channel] is blank the bot will answer the question in the channel the " \
@@ -24,17 +25,21 @@ def eightball(inp, conn=None, chan=None, nick=''):
     magic = multiword_replace(random.choice(responses), color_codes)
 
     inp = inp.split(" ")
-    
     if inp[0][0] == "#":
-	message = "(" + nick + ") "
-        for x in inp[1:]:
-            message = message + x + " "
-	message = message[:-1]:
-	out = "PRIVMSG %s :%s" % (inp[0], message)
-	conn.send(out)
-	message = "the magic 8 ball says..." + magic
-        out = "PRIVMSG %s :%s" % (inp[0], message)
+        admins = bot.config.get('admins', [])
+        if nick not in admins and mask not in admins:
+	    notice("Sorry, you are not allowed to use this command.")
+        else:
+    	    message = "(" + nick + ") "
+            for x in inp[1:]:
+                message = message + x + " "
+	    message = message[:-1]
+	    out = "PRIVMSG %s :%s" % (inp[0], message)
+	    conn.send(out)
+	    message = "the magic 8 ball says..." + magic
+            out = "PRIVMSG %s :%s" % (inp[0], message)
+	    conn.send(out)
     else:
 	message = "the magic 8 ball says..." + magic
         out = "PRIVMSG %s :%s" % (chan, message)
-    conn.send(out)
+        conn.send(out)
