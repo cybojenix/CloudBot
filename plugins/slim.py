38796cbe8d5ae320e29c1cc911218ae80ced5b97
CafogenMod <znc@cafogen.org>
2013-07-17 23:41:51 +0400

    add in a device list of all the official support slim devices also now use separate functions for slim

--- plugins/slim.py
+++ plugins/slim.py
@@ -1,25 +1,41 @@
 import os
-
+import urllib
+import xml.etree.ElementTree as ET
 from util import hook
 
-@hook.command('slim')
-def slim(inp, conn=None, chan=None, nick=''):
-        "slim [option] -- options are 'version' " \
-        "more to come!"
-        inp = inp.split(" ")
-        if inp[0][0] == "v":
-                os.chmod('plugins/data/slim-version.sh', 0777)
-                os.system('./plugins/data/slim-version.sh')
-                with open('plugins/data/version-bridge') as f:
-                        official_version = f.readline()
-                        weekly_version = f.readline()
+@hook.command(autohelp=False)
+def slimversion(inp, conn=None, chan=None, nick=''):
+    "slimversion - output the current versions of slimrom"
+	
+    os.chmod('plugins/data/slim-version.sh', 0777)
+    os.system('./plugins/data/slim-version.sh')
+    with open('plugins/data/version-bridge') as f:
+        official_version = f.readline()
+        weekly_version = f.readline()
 
-                official_version = official_version.strip()
-                weekly_version = weekly_version.strip()
+    official_version = official_version.strip()
+    weekly_version = weekly_version.strip()
 
-                message = "(" + nick + ") The current official version is version " + official_version
-                out = "PRIVMSG %s :%s" % (chan, message)
-                conn.send(out)
-                message = "(" + nick + ") The current weekly version is version " + weekly_version
-                out = "PRIVMSG %s :%s" % (chan, message)
-                conn.send(out)
+    message = "(" + nick + ") The current official version is version " + official_version
+    out = "PRIVMSG %s :%s" % (chan, message)
+    conn.send(out)
+    message = "(" + nick + ") The current weekly version is version " + weekly_version
+    out = "PRIVMSG %s :%s" % (chan, message)
+    conn.send(out)
+
+@hook.command(autohelp=False)
+def slimdevices(inp, conn=None, chan=None, nick=''):
+	"slimdevices - output the currently supported devices for slimrom"
+
+	ota_xml = urllib.urlretrieve("http://otaslim.slimroms.net/ota.xml", "plugins/data/ota.xml")
+
+	root = ET.parse('plugins/data/ota.xml').getroot()
+	message = "(" + nick + ") devices support are.. "
+	
+	for child in root[0]:
+		message = message + child.tag + ", "
+		
+	os.remove('plugins/data/ota.xml')
+	
+	out = "PRIVMSG %s :%s" % (chan, message)
+	conn.send(out)
