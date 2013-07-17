2e281455c09b7a655bf3dbc4592c4e1a47ef968d
CafogenMod <znc@cafogen.org>
2013-07-18 00:00:13 +0400

    touch up some stuff, and only include official devices

--- plugins/slim.py
+++ plugins/slim.py
@@ -25,17 +25,18 @@ def slimversion(inp, conn=None, chan=None, nick=''):
 
 @hook.command(autohelp=False)
 def slimdevices(inp, conn=None, chan=None, nick=''):
-	"slimdevices - output the currently supported devices for slimrom"
+	"slimdevices - output the officially supported devices for slimrom"
 
 	ota_xml = urllib.urlretrieve("http://otaslim.slimroms.net/ota.xml", "plugins/data/ota.xml")
 
 	root = ET.parse('plugins/data/ota.xml').getroot()
-	message = "(" + nick + ") devices support are.. "
+	message = "(" + nick + ") officially supported devices are.. "
 	
 	for child in root[0]:
+	    if not "UNOFFICIAL" in child[0].text:
 		message = message + child.tag + ", "
 		
 	os.remove('plugins/data/ota.xml')
 	
-	out = "PRIVMSG %s :%s" % (chan, message)
+	out = "PRIVMSG %s :%s" % (chan, message[:-2])
 	conn.send(out)
