8bcae07852fe01851931dc4814de42c9f4972a0a
CafogenMod <znc@cafogen.org>
2013-07-19 20:09:47 +0400

    finish up the notifier, remove the dirty bash script, and allow the slimversion function to fetch the values from the notifier

--- plugins/slim.py
+++ plugins/slim.py
@@ -3,21 +3,31 @@ import os
 import urllib
 import xml.etree.ElementTree as ET
 from util import hook
-import re
+import re, mmap
 
 @hook.singlethread
 @hook.event('*')
 def slimtest(inp, conn=None):
 	urllib.urlretrieve("http://otaslim.slimroms.net/ota.xml", "plugins/data/ota.xml")
+	urllib.urlretrieve("http://www.slimroms.net/index.php/downloads/all/viewcategory/624-grouper", "plugins/data/624-grouper")
 	root = ET.parse('plugins/data/ota.xml').getroot()
 	global official_version
+	global weekly_version
 	
+	# officials
 	for child in root[0].findall('grouper'):
 		cur_official_version = child[0].text
 	
 	m = re.search('\d+(\.\d+)*-', cur_official_version)
 	cur_official_version = m.group(0)[:-1]
 	
+	#weeklies
+	with open('plugins/data/624-grouper', 'r+') as f:
+		data = mmap.mmap(f.fileno(), 0)
+		mo = re.search('Slim-grouper.*-WEEKLY', data)
+	mot = re.search('\d+(\.\d+)*-', mo.group(0))
+	cur_weekly_version = mot.group(0)[:-1]
+
 	try:
 		official_version
 	except NameError:
@@ -32,61 +42,46 @@ def slimtest(inp, conn=None):
 				message = "a new version of slimrom is is released, version " + official_version
 				out = "NOTICE %s :%s" % (channel, message)
 				conn.send(out)
-	time.sleep(300)
 
-#@hook.singlethread
-#@hook.event('*')
-#def slimtest(inp, conn=None):
-#    channel = "#cafogen"
-#    message = "test, sorry"
-#    out = "NOTICE %s :%s" % (channel, message)
-#    conn.send(out)
-#    time.sleep(300)
+	try:
+		weekly_version
+	except NameError:
+		weekly_version = cur_weekly_version
+	else:
+#		if re.search('\d+\.\d+(\.\d+)+', weekly_version) or \ # will come back to this later
+#		re.search('\d+\.\d+(\.\d+)+', cur_weekly_version):
+			if not weekly_version == cur_weekly_version:
+				weekly_version = cur_weekly_version
+				
+				channel = "#cafogen"
+				message = "a new weekly version of slimrom is is released, version " + official_version
+				out = "NOTICE %s :%s" % (channel, message)
+				conn.send(out)
+	time.sleep(60)
 
 @hook.command(autohelp=False)
-def slimversion(inp, conn=None, chan=None, nick=''):
+def slimversion(inp, say=None):
     "slimversion - output the current versions of slimrom"
 	
-    os.chmod('plugins/data/slim-version.sh', 0777)
-    os.system('./plugins/data/slim-version.sh')
-    with open('plugins/data/version-bridge') as f:
-        official_version = f.readline()
-        weekly_version = f.readline()
-
-    official_version = official_version.strip()
-    weekly_version = weekly_version.strip()
-
-    message = "(" + nick + ") The current official version is version " + official_version
-    out = "PRIVMSG %s :%s" % (chan, message)
-    conn.send(out)
-    message = "(" + nick + ") The current weekly version is version " + weekly_version
-    out = "PRIVMSG %s :%s" % (chan, message)
-    conn.send(out)
+    say("The current official version is version #" + official_version)
+    say("The current weekly version is version #" + weekly_version)
 
 @hook.command(autohelp=False)
-def slimdevices(inp, conn=None, chan=None, nick=''):
+def slimdevices(inp, say=None):
 	"slimdevices - output the officially supported devices for slimrom"
 
-	ota_xml = urllib.urlretrieve("http://otaslim.slimroms.net/ota.xml", "plugins/data/ota.xml")
-
 	root = ET.parse('plugins/data/ota.xml').getroot()
-	message = "(" + nick + ") officially supported devices are.. "
+	message = "officially supported devices are.. "
 	
 	for child in root[0]:
 	    if not "UNOFFICIAL" in child[0].text:
 		message = message + child.tag + ", "
-		
-	os.remove('plugins/data/ota.xml')
-	
-	out = "PRIVMSG %s :%s" % (chan, message[:-2])
-	conn.send(out)
+	say(message[:-2])
 
 @hook.command		
-def slimsearch(inp, conn=None, chan=None, nick=''):
+def slimsearch(inp, notice=None, say=None):
 	"slimsearch - query the list of devices to get the url from the website," \
 	" for a list of devices, run slimdevices"
-	
-	urllib.urlretrieve("http://otaslim.slimroms.net/ota.xml", "plugins/data/ota.xml")
 	
 	inp = inp.split(" ")
 	search = inp[0]
@@ -99,11 +94,8 @@ def slimsearch(inp, conn=None, chan=None, nick=''):
 	try:
 		output_url
 	except NameError:
-		message =  search + " not found, please run the slimdevices command"
+		message = search + " not found, please run the slimdevices command"
 		notice(message)
 	else:
-		message =  "(" + nick + ") " + search + ": " + output_url
-		out = "PRIVMSG %s :%s" % (chan, message)
-		conn.send(out)
-		
-	os.remove('plugins/data/ota.xml')
+		message = search + ": " + output_url
+		say(message)
