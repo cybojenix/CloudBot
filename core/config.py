de1ef2cc4baf9519c9e8c548a7d1b28b9ac04506
Cybo Jenix <anthonydking@gmail.com>
2013-07-22 19:55:37 +0100

    add in an example of how to set out the keys

--- core/config.py
+++ core/config.py
@@ -20,7 +20,7 @@ if not os.path.exists('config'):
               "realname": "CafoBot - http://git.io/KczkZw",
               "nickserv_password": "",
               "channels": ["#cafogen"],
-	      "channelkeys": {},
+	      "channelkeys": {"#cafogen": "example"},
               "invite_join": true,
               "auto_rejoin": false,
               "command_prefix": "."
