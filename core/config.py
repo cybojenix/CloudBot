0d9f1849593ed0bfdd30263037b820c700f9c97b
Cybo Jenix <anthonydking@gmail.com>
2013-07-29 13:32:00 +0100

    Update config.py

--- core/config.py
+++ core/config.py
@@ -20,7 +20,6 @@ if not os.path.exists('config'):
               "realname": "CafoBot - http://git.io/KczkZw",
               "nickserv_password": "",
               "channels": ["#cafogen"],
-	      "channelkeys": {},
               "invite_join": true,
               "auto_rejoin": false,
               "command_prefix": "."
