d7255dd99608f98592f0bfed99346e368f7e6cd1
cybojenix <anthonydking@gmail.com>
2013-08-07 14:24:18 +0100

    update correction plugin to use a database instead of json files

--- plugins/correction.py
+++ plugins/correction.py
@@ -1,45 +1,41 @@
 from util import hook
-import json
 import re
-import os
 
+db_ready=False
+
+# from seen.py
+def db_init(db):
+    "check to see that our db has the the correction table and return a connection."
+    db.execute("create table if not exists correct_user(name, quote, chan, "
+                 "primary key(name, chan))")
+    db.commit()
+    db_ready = True
+
+@hook.singlethread
 @hook.event('PRIVMSG')
-def catchmessage(inp, input=None):
-        dir = "plugins/data/correction/"
-        file = dir + input.chan
-        if not os.path.exists(dir):
-                os.makedirs(dir)
-	if not os.path.exists(file):
-		f = open(file, 'w+')
-		f.close()
-        if not re.findall('^s/.*/.*/$', input.msg.lower()):
-		if not os.stat(file)[6]==0:
-			with open(file, 'r') as f:
-				data = json.load(f)
-		else:
-			data = {}
-        	with open(file, 'w') as f:
-               		data[str(input.nick)] = str(input.msg)
-			json.dump(data, f, ensure_ascii=False)
+def message_sieve(paraml, input=None, db=None, bot=None):
+    if not db_ready:
+        db_init(db)
+    if not re.findall('^s/.*/.*/$', input.msg.lower()):
+        db.execute("insert or replace into correct_user(name, quote, chan)"
+            "values(?,?,?)", (input.nick.lower(), input.msg, input.chan.lower()))
+        db.commit()
 
-@hook.regex(r'^S/.*/.*/$')
-@hook.regex(r'^s/.*/.*/$')
-def correction(inp, say=None, nick=None, input=None, notice=None):
-        dir = "plugins/data/correction/"
-        file = dir + input.chan
-	if not os.path.exists(file):
-		notice("I haven't seen anyone say anything on this channel yet")
-	else:
-		splitinput = input.msg.split("/")
-		find = splitinput[1]
-		replace = splitinput[2]
+@hook.regex(r'^(s|S)/.*/.*/$')
+def correction(inp, say=None, input=None, notice=None, db=None):
+    if not db_ready:
+        db_init(db)
 
-		with open(file, 'r') as f:
-                	readdata = json.load(f)
-		try:
-			said = readdata[input.nick]
-		except KeyError:
-			notice("I haven't seen you say anything here yet")
-		else:
-			if splitinput[1] in said:
-				say("%s meant to say: %s" % (input.nick, said.replace(find, replace)))
+    last_message = db.execute("select name, quote from correct_user where name"
+                           " like ? and chan = ?", (input.nick.lower(), input.chan.lower())).fetchone()
+
+    if last_message:
+        splitinput = input.msg.split("/")
+        find = splitinput[1]
+        replace = splitinput[2]
+        if find in last_message[1]:
+            say("%s meant to say: %s" % (input.nick, last_message[1].replace(find, replace)))
+        else:
+            notice("%s can't be found in your last message" % find)
+    else:
+        notice("I haven't seen you say anything here yet")
