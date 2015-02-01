8514e788b1121297e0ad9338c8b7e410eb815752
cybojenix <anthonydking@slimroms.net>
2015-02-01 22:25:43 +0100

    GermainZ you pedant.

--- plugins/pulser.py
+++ plugins/pulser.py
@@ -3,13 +3,13 @@ from time import time
 
 _pulser_last_pinged = None
 
-@hook.regex(r'[cC][lL][oO][uU][dD]')
+@hook.regex(r'.*(?i)cloud.*')
 def pulser(match, message=None, nick=None):
     global _pulser_last_pinged
-    if nick == "pulser":
+    if nick.lower() == "pulser":
         return
     t = time()
     if _pulser_last_pinged and _pulser_last_pinged - t < 60*5:
         return
     _pulser_last_pinged = t
-    message(u"Pulser, %s is talking about Clouds!!" % nick)
+    message(u"pulser, %s is talking about Clouds!!" % nick)
