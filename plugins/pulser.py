38089a84d317fb82bb7740fc96ad524a72172d97
cybojenix <anthonydking@slimroms.net>
2015-02-02 16:00:40 +0100

    Per channel detection

--- plugins/pulser.py
+++ plugins/pulser.py
@@ -1,15 +1,15 @@
 from util import hook
 from time import time
 
-_pulser_last_pinged = None
+_pulser_last_pinged = {}
 
 @hook.regex(r'.*(?i)cloud.*')
-def pulser(match, message=None, nick=None):
+def pulser(match, message=None, nick=None, chan=None):
     global _pulser_last_pinged
     if nick.lower() == "pulser":
         return
     t = time()
-    if _pulser_last_pinged and t - _pulser_last_pinged < 60*5:
+    if t - _pulser_last_pinged.get(chan.lower(), 0) < 60*5:
         return
-    _pulser_last_pinged = t
+    _pulser_last_pinged[chan.lower()] = t
     message(u"pulser, %s is talking about Clouds!!" % nick)
