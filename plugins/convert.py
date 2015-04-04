26f5dc741c728c283361b22589e12fe56bf83097
cybojenix <anthonydking@slimroms.net>
2015-04-04 02:30:33 +0200

    use new provider for currency conversions
    it's free for 1000 calls, but at least it stays up

--- plugins/convert.py
+++ plugins/convert.py
@@ -1,18 +1,41 @@
 from util import hook, http
+from time import time
 
-convert_url = "http://rate-exchange.appspot.com/currency?from={}&to={}&q=1"
+_conv_data = {}
+_conv_last_edit = None
+
+
+def load_data(api_key):
+    global _conv_data
+    global _conv_last_edit
+
+    if _conv_last_edit is not None and _conv_data and _conv_last_edit - time() < 1800:
+        return _conv_data
+
+    _conv_last_edit = time()
+    url = "https://openexchangerates.org/api/latest.json?app_id={}".format(api_key)
+    _conv_data = http.get_json(url)
+    return _conv_data
+
 
 @hook.command
-def convert(inp):
+def convert(inp, bot=None):
     """
     convert <from> <to> [amount] -- convert [amount] <from> to <to>
     """
+    api_key = bot.config.get("api_keys", {}).get("openexchangerates")
+    if not api_key:
+        return "api key not added"
 
-    input = inp.lower().strip()
+    input = inp.upper().strip()
     parts = input.split()
 
-    ffrom = parts[0]
-    to = parts[1]
+    try:
+        ffrom = parts[0]
+        to = parts[1]
+    except IndexError:
+        return "invalid arguments"
+
     try:
         amount = float(parts[2])
     except IndexError:
@@ -20,14 +43,19 @@ def convert(inp):
     except ValueError:
         return "only use numbers for the quantity"
 
-    page = http.get_json(convert_url.format(ffrom, to))
-    if page.get("err", None):
-        return "invalid currency"
-    if page.get("warning", None):
-        return "invalid quantity"
-    value = page.get("v", None)
-    if not value:
-        return "dafuq just happened..."
-    value = float(value) * float(amount)
-    return_statement = "{:.2f} {} is {:.2f} {}".format(amount, ffrom, value, to)
-    return return_statement
+    data = load_data(api_key)
+    if not data:
+        return "internal error: data not loaded"
+    rates = data.get("rates", {})
+    if not rates:
+        return "internal error: rates not loaded"
+
+    f = rates.get(ffrom)
+    if f is None:
+        return "base currency not known"
+    t = rates.get(to)
+    if t is None:
+        return "target currency not known"
+
+    value = float(amount)/(float(f)*float(t))
+    return "{:.2f} {} is {:.2f} {}".format(amount, ffrom, value, to)
